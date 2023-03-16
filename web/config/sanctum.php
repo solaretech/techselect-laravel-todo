cc2f0ff2259483a0350b1700075c513df2ff4db5
solaretech <solaretech.kk@gmail.com>
2023-03-16 09:19:04 +0900

    認証エラー解消のため、sanctum.phpの編集

--- web/config/sanctum.php
+++ web/config/sanctum.php
@@ -18,8 +18,10 @@ return [
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'techselect-laravel-todo.onrender.com',
         Sanctum::currentApplicationUrlWithPort()
-    ))),
+    )
+    )),
 
     /*
     |--------------------------------------------------------------------------
